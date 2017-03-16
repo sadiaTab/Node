2a1f14e4438e7e48dd1593a3de3fa24ae7be2043
devzden <tabassum1234@gmail.com>
2017-03-16 20:13:48 +0000

    Express

--- app3.js
+++ app3.js
@@ -2,8 +2,22 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res) {
+    if(req.url === '/'){
+        fs.createReadStream(__dirname + '/index.htm', 'utf8').pipe(res);
+    }
+    else if(req.url === '/api'){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        var obj = {
+            firstname: 'John',
+            lastname: 'Does'
+        }
+        res.end(JSON.stringify(obj));
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream(__dirname + '/index.htm', 'utf8').pipe(res);
+    }
+    
+    else{
+        res.writeHead(404);
+        res.end();
+    }
     
 }).listen(1337, '127.0.0.1');
